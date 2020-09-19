0351f5dbaef823b3ee90667e41b4fde8a80c0383
Dane4ka <40688428+danyaffff@users.noreply.github.com>
2020-09-19 15:39:52 +0300

    Update 7.sh

--- Sem 3, Lab 1/7.sh
+++ Sem 3, Lab 1/7.sh
@@ -1,1 +1,1 @@
-grep -E -h -I -i -o -r -s "[[:alnum:]_-]+@[[:alnum:]]+\.([[:alnum:]]+){1}" "/etc" > "${PWD}/emails.lst"
+grep -E -h -I -i -o -r -s "[[:alnum:]_-]+@[[:alnum:]]+\.([[:alnum:]]+){1}" "/etc" | tr -s "\n" "," > "${PWD}/emails.lst"
