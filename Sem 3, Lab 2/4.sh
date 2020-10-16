66402600f4d7cd9900c3c5f21a54d0057890dd89
Dane4ka <40688428+danyaffff@users.noreply.github.com>
2020-10-16 21:46:29 +0300

    Update 4.sh

--- Sem 3, Lab 2/4.sh
+++ Sem 3, Lab 2/4.sh
@@ -1,1 +1,20 @@
-for proc in $(ps ax -o pid); do    pid=$(grep -P -s "^Pid:" /proc/$proc/status | grep -s -o -P '\d*')    ppid=$(grep -P -s "^PPid:" /proc/$proc/status | grep -s -o -P '\d*')    sum_exec_runtime=$(grep -P -s "se.sum_exec_runtime" /proc/$proc/sched | awk '{print $3}')	nr_switches=$(grep -P -s "nr_switches" /proc/$proc/sched | grep -o -s -P '\d*')	if [[ -z $pid ]]; then		continue;	fi	if [[ -z $ppid ]]; then		ppid=0	fi		art=$(echo "scale=2; x=$sum_exec_runtime/$nr_switches; if(x<1&&x!=0) print 0; x" | bc)    echo "ProcessID=$pid : Parent_ProcessID=$ppid : Averange_Running_Time=$art"done |    sort --key=2 -V > out4.txt
+for proc in $(ps ax -o pid); do
+    pid=$(grep -P -s "^Pid:" /proc/$proc/status | grep -s -o -P '\d*')
+    ppid=$(grep -P -s "^PPid:" /proc/$proc/status | grep -s -o -P '\d*')
+    sum_exec_runtime=$(grep -P -s "se.sum_exec_runtime" /proc/$proc/sched | awk '{print $3}')
+	nr_switches=$(grep -P -s "nr_switches" /proc/$proc/sched | grep -o -s -P '\d*')
+
+	if [[ -z $pid ]]; then
+		continue;
+	fi
+
+	if [[ -z $ppid ]]; then
+		ppid=0
+	fi	
+
+	art=$(echo "scale=2; x=$sum_exec_runtime/$nr_switches; if(x<1&&x!=0) print 0; x" | bc)
+
+    echo "ProcessID=$pid : Parent_ProcessID=$ppid : Averange_Running_Time=$art"
+done |
+    sort --key=2 -V > out4.txt
+    
