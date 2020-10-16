cfa3d6500d2651b9ec3237998f14725998d62243
Dane4ka <40688428+danyaffff@users.noreply.github.com>
2020-10-16 21:46:50 +0300

    Update 5.sh

--- Sem 3, Lab 2/5.sh
+++ Sem 3, Lab 2/5.sh
@@ -1,1 +1,22 @@
-previous_ppid=-1total_art=0counter=0while read line ; do    ppid=$(echo $line | cut -d ":" -f 2 | awk -F "=" '{print $2}')    art=$(echo $line | cut -d ":" -f 3 | awk -F "=" '{print $2}')	#echo $ppid	#echo $previous_ppid	if [[ $previous_ppid != -1 && $ppid != $previous_ppid ]]; then		echo Average_Running_Children_of_ParentID=$previous_ppid is $(echo "scale=2; x=$total_art / $counter; if(x<1&&x!=0) print 0; x" | bc)        acc_avg_sleep=0        process_counter=0	fi	echo $line	total_art=$(echo "scale=2; x=$total_art+$art; if(x<1&&x!=0) print 0; x" | bc)	((counter+=1))	previous_ppid=$ppiddone < out4.txt > out5.txt
+previous_ppid=-1
+total_art=0
+counter=0
+
+while read line ; do
+    ppid=$(echo $line | cut -d ":" -f 2 | awk -F "=" '{print $2}')
+    art=$(echo $line | cut -d ":" -f 3 | awk -F "=" '{print $2}')
+	#echo $ppid
+	#echo $previous_ppid
+
+	if [[ $previous_ppid != -1 && $ppid != $previous_ppid ]]; then
+		echo Average_Running_Children_of_ParentID=$previous_ppid is $(echo "scale=2; x=$total_art / $counter; if(x<1&&x!=0) print 0; x" | bc)
+        acc_avg_sleep=0
+        process_counter=0
+	fi
+
+	echo $line
+
+	total_art=$(echo "scale=2; x=$total_art+$art; if(x<1&&x!=0) print 0; x" | bc)
+	((counter+=1))
+	previous_ppid=$ppid
+done < out4.txt > out5.txt
