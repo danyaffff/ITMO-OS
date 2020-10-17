a8d857a8e7514ee57195a1ff485e7aa8551a9921
Dane4ka <40688428+danyaffff@users.noreply.github.com>
2020-10-17 13:55:04 +0300

    Update 5.sh

--- Sem 3, Lab 2/5.sh
+++ Sem 3, Lab 2/5.sh
@@ -6,15 +6,15 @@ while read line ; do
     ppid=$(echo $line | cut -d ":" -f 2 | awk -F "=" '{print $2}')
     art=$(echo $line | cut -d ":" -f 3 | awk -F "=" '{print $2}')
 
-	if [[ $previous_ppid != -1 && $ppid != $previous_ppid ]]; then
-		echo Average_Running_Children_of_ParentID=$previous_ppid is $(echo "scale=2; x=$total_art / $counter; if(x<1&&x!=0) print 0; x" | bc)
+    if [[ $previous_ppid != -1 && $ppid != $previous_ppid ]]; then
+	echo Average_Running_Children_of_ParentID=$previous_ppid is $(echo "scale=2; x=$total_art / $counter; if(x<1&&x!=0) print 0; x" | bc)
         acc_avg_sleep=0
         process_counter=0
-	fi
+    fi
 
-	echo $line
+    echo $line
 
-	total_art=$(echo "scale=2; x=$total_art+$art; if(x<1&&x!=0) print 0; x" | bc)
-	((counter+=1))
-	previous_ppid=$ppid
+    total_art=$(echo "scale=2; x=$total_art+$art; if(x<1&&x!=0) print 0; x" | bc)
+    ((counter+=1))
+    previous_ppid=$ppid
 done < out4.txt > out5.txt
